dbd54e4a3d30199655242e2d658a21341d8443ba
Grigory Davydov <gdavidov22@gmail.com>
2025-10-27 14:10:12 +0300

    modified method that removes item from memory% was GET, now: DELETE

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-"""
-from fastapi import FastAPI, Request
-app = FastAPI()
-
-#Defines a GET endpoint at /hh/callback using the @app.get() decorator
-@app.get("/hh/callback")
-#Defines an async function that accepts a Request object as an argument
-async def callback(request: Request):
-    #Extracts the code and state parameters from the query string of the incoming request
-    code = request.query_params.get("code")
-    state = request.query_params.get("state")
-    #Returns a dictionary with the code and state parameters
-    return {"code": code, "state": state}
-"""
-
-
-
 # main.py
 import os, time, json
 from collections import deque
@@ -62,7 +45,7 @@ def admin_pending(admin_token: str):
     return JSONResponse(list(pending))
 
 # Remove item from memoryby state
-@app.get("/admin/dequeue")
+@app.delete("/admin/dequeue")
 def admin_dequeue(admin_token: str, state: str):
     if admin_token != ADMIN_TOKEN:
         raise HTTPException(status_code=401, detail="Unauthorized")
